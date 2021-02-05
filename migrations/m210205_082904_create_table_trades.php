8f4ff2a5c8a51222f7da99626bd4134872b22da6
Administrator <imarketing24.pro@gmail.com>
2021-02-05 09:41:08 -0800

    readme add info

--- migrations/m210205_082904_create_table_trades.php
+++ migrations/m210205_082904_create_table_trades.php
@@ -1,39 +1,42 @@
 <?php
 
-use yii\db\Migration;
-
-/**
- * Handles the creation for table `{{%trades}}`.
- */
-class m210205_082904_create_table_trades extends Migration
-{
-    /**
-     * @inheritdoc
-     */
-    public function safeUp()
-    {
-        $this->createTable('{{%trades}}', [
-
-            'id' => $this->primaryKey()->notNull(),
-            'ticket' => $this->integer(11),
-            'login' => $this->integer(11),
-            'symbol' => $this->string(15),
-            'cmd' => $this->integer(11),
-            'volume' => $this->float(),
-            'open_time' => $this->datetime(),
-            'close_time' => $this->datetime(),
-            'profit' => $this->float(),
-            'coeff_h' => $this->float(),
-            'coeff_cr' => $this->float(),
-
-        ]);
-     }
+    use yii\db\Migration;
 
     /**
-     * @inheritdoc
+     * Handles the creation for table `{{%trades}}`.
      */
-    public function safeDown()
+    class m210205_082904_create_table_trades extends Migration
     {
-        $this->dropTable('{{%trades}}');
+        /**
+         * @inheritdoc
+         */
+        public function safeUp()
+        {
+            $this->createTable(
+                '{{%trades}}',
+                [
+
+                    'id' => $this->primaryKey()->notNull(),
+                    'ticket' => $this->integer(11),
+                    'login' => $this->integer(11),
+                    'symbol' => $this->string(15),
+                    'cmd' => $this->integer(11),
+                    'volume' => $this->float(),
+                    'open_time' => $this->datetime(),
+                    'close_time' => $this->datetime(),
+                    'profit' => $this->float(),
+                    'coeff_h' => $this->float(),
+                    'coeff_cr' => $this->float(),
+
+                ]
+            );
+        }
+
+        /**
+         * @inheritdoc
+         */
+        public function safeDown()
+        {
+            $this->dropTable('{{%trades}}');
+        }
     }
-}
--- migrations/m210205_082916_create_table_accounts.php
+++ migrations/m210205_082916_create_table_accounts.php
@@ -1,31 +1,34 @@
 <?php
 
-use yii\db\Migration;
-
-/**
- * Handles the creation for table `{{%accounts}}`.
- */
-class m210205_082916_create_table_accounts extends Migration
-{
-    /**
-     * @inheritdoc
-     */
-    public function safeUp()
-    {
-        $this->createTable('{{%accounts}}', [
-
-            'id' => $this->primaryKey()->notNull(),
-            'client_uid' => $this->integer(11),
-            'login' => $this->integer(11),
-
-        ]);
-     }
+    use yii\db\Migration;
 
     /**
-     * @inheritdoc
+     * Handles the creation for table `{{%accounts}}`.
      */
-    public function safeDown()
+    class m210205_082916_create_table_accounts extends Migration
     {
-        $this->dropTable('{{%accounts}}');
+        /**
+         * @inheritdoc
+         */
+        public function safeUp()
+        {
+            $this->createTable(
+                '{{%accounts}}',
+                [
+
+                    'id' => $this->primaryKey()->notNull(),
+                    'client_uid' => $this->integer(11),
+                    'login' => $this->integer(11),
+
+                ]
+            );
+        }
+
+        /**
+         * @inheritdoc
+         */
+        public function safeDown()
+        {
+            $this->dropTable('{{%accounts}}');
+        }
     }
-}
